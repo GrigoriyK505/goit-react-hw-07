f683fdeaff5ff795bbe7eb746d9825b62f14ccec
Anastasia_S <sivokonanastasia1@gmail.com>
2025-03-01 21:05:27 +0200

    ain

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,29 +2,17 @@ import s from './App.module.css';
 import ContactForm from './ContactForm/ContactForm';
 import SearchBox from './SearchBox/SearchBox';
 import ContactList from './ContactList/ContactList';
-// import {useEffect } from 'react';
-// import axios from 'axios';
-// import toast from 'react-hot-toast';
+import {useEffect } from 'react';
+import { fetchContacts } from '../redux/contactsOps';
+import { useDispatch } from 'react-redux';
 
 const App = () => {
-//   useEffect(() => {
-//     const abortController = new AbortController()
+  const dispatch = useDispatch();
 
-//     axios
-//       .get('https://dummyjson.com/test', {signal: abortController.signal})
-//       .then(res => console.log(res.data))
-//       .catch(error => {
-//         if (axios.isCancel(error)) {
-//           console.log('Cancel with abort controller');
-//           toast.error('Cancel abort controller');
-//         }
-//       console.log(error);
-//       });
-    
-//     return () => {
-//       abortController.abort()
-//     }
-// }, []);
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+  
   return (
     <div className={s.container}>
       <h1>Phonebook</h1>
